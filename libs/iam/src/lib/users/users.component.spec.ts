4868afee809243843085861262d3310f18f4e4cd
Nicolas Lagier <nicolas.lagier@gmail.com>
2020-11-17 15:36:17 +0800

    style

--- libs/iam/src/lib/users/users.component.spec.ts
+++ libs/iam/src/lib/users/users.component.spec.ts
@@ -1,3 +1,5 @@
+import { of } from 'rxjs';
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -7,7 +9,7 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { of } from 'rxjs';
+
 import { UsersAdapter } from '../users.adapter';
 import { UsersComponent } from './users.component';
 
