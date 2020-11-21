b643b539a494fbb7399a01078f81c2f55ff8f9b5
Nicolas Lagier <nicolas.lagier@gmail.com>
2020-11-21 14:31:30 +0800

    Fix more tests

--- libs/iam/src/lib/components/group-list/group-list.component.spec.ts
+++ libs/iam/src/lib/components/group-list/group-list.component.spec.ts
@@ -1,16 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatTableModule } from '@angular/material/table';
 import { TableModule } from '@cnfs/angular-table';
+import { GroupsAdapter } from '../../services/groups.adapter';
 import { GroupListComponent } from './group-list.component';
+import { of } from 'rxjs';
 
 describe('GroupListComponent', () => {
   let component: GroupListComponent;
   let fixture: ComponentFixture<GroupListComponent>;
-
+  const groupsAdapterMock: Partial<GroupsAdapter> = {
+    getTableData: () =>
+      of({
+        data: [],
+        meta: {},
+      }),
+  };
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [GroupListComponent],
       imports: [TableModule, MatTableModule],
+      providers: [{ provide: GroupsAdapter, useValue: groupsAdapterMock }],
     }).compileComponents();
   });
 
