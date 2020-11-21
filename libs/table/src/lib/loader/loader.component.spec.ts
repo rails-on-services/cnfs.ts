0b3672a7f7aad977a004030c5cb2e031997e49e9
Nicolas Lagier <nicolas.lagier@gmail.com>
2020-11-21 14:01:23 +0800

    Fix broken test

--- libs/table/src/lib/loader/loader.component.spec.ts
+++ libs/table/src/lib/loader/loader.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { LoaderComponent } from './loader.component';
+import { MatIconModule } from '@angular/material/icon';
 
 describe('LoaderComponent', () => {
   let component: LoaderComponent;
@@ -8,9 +9,9 @@ describe('LoaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoaderComponent ]
-    })
-    .compileComponents();
+      declarations: [LoaderComponent],
+      imports: [MatProgressBarModule, MatIconModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
