5b4141f3105d67c2e28e27fb12aeae985aa97f2d
Nicolas Lagier <nicolas.lagier@gmail.com>
2020-11-24 15:21:21 +0800

    fix tests

--- libs/cognito/src/lib/cognito.module.ts
+++ libs/cognito/src/lib/cognito.module.ts
@@ -1,20 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
 import { RouterModule } from '@angular/router';
 import { CnfsCommonModule } from '@cnfs/common';
-import { HomeComponent } from './pages/home/home.component';
-import { routes } from './routes';
+import { CustomerEditComponent } from './components/customer-edit/customer-edit.component';
+import { CustomerListComponent } from './components/customer-list/customer-list.component';
+import { PoolEditComponent } from './components/pool-edit/pool-edit.component';
+import { PoolListComponent } from './components/pool-list/pool-list.component';
 import { CustomersComponent } from './pages/customers/customers.component';
-import { PoolsComponent } from './pages/pools/pools.component';
 import { EditCustomerComponent } from './pages/edit-customer/edit-customer.component';
 import { EditPoolComponent } from './pages/edit-pool/edit-pool.component';
-import { CustomerListComponent } from './components/customer-list/customer-list.component';
-import { PoolListComponent } from './components/pool-list/pool-list.component';
-import { CustomerEditComponent } from './components/customer-edit/customer-edit.component';
-import { PoolEditComponent } from './components/pool-edit/pool-edit.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PoolsComponent } from './pages/pools/pools.component';
+import { routes } from './routes';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes), CnfsCommonModule],
   declarations: [
     HomeComponent,
     CustomersComponent,
@@ -25,6 +25,12 @@ import { PoolEditComponent } from './components/pool-edit/pool-edit.component';
     PoolListComponent,
     CustomerEditComponent,
     PoolEditComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    CnfsCommonModule,
+    MatTabsModule,
   ],
 })
 export class CognitoModule {}
--- libs/cognito/src/lib/pages/home/home.component.ts
+++ libs/cognito/src/lib/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ILink } from '@cnfs/common';
 
 @Component({
   selector: 'cnfs-home',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  // public
+  public links: ILink[] = [
+    { label: 'Customers', route: 'customers' },
+    { label: 'Pools', route: 'pools' },
+  ];
 }
