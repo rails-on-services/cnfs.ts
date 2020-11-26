7db6605087edd75a8b0a9eb98201b8bf13957df6
Nicolas Lagier <jihoun@users.noreply.github.com>
2020-11-26 14:32:25 +0800

    pool-edit (#20)

--- libs/cognito/src/lib/pages/pools/pools.component.ts
+++ libs/cognito/src/lib/pages/pools/pools.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
-
+import { Router, ActivatedRoute } from '@angular/router';
+import { IPool } from '../../models/pool';
 @Component({
   selector: 'cnfs-pools',
   templateUrl: './pools.component.html',
   styleUrls: ['./pools.component.scss'],
 })
-export class PoolsComponent {}
+export class PoolsComponent {
+  public constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  public onAction(evt: { action: string; pool: IPool }): void {
+    const { action, pool } = evt;
+    if (action === 'edit') {
+      this.router.navigate(['..', 'pool-edit', pool.id], {
+        relativeTo: this.activatedRoute,
+      });
+    }
+  }
+}
