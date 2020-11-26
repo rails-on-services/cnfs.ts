f0f9d99f4f2e5c0b1df30f4fa4cf5674efa0800c
Nicolas Lagier <nicolas.lagier@gmail.com>
2020-11-26 14:27:12 +0800

    pool-edit

--- libs/cognito/src/lib/components/pool-list/pool-list.component.ts
+++ libs/cognito/src/lib/components/pool-list/pool-list.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input } from '@angular/core';
-import { PoolsAdapter } from '../../services/pools.adapter';
-import { IPool } from '../../models/pool';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CustomDataSource } from '@cnfs/angular-table';
+import { IPool } from '../../models/pool';
+import { PoolsAdapter } from '../../services/pools.adapter';
+import { IAction } from '@cnfs/common';
+
 @Component({
   selector: 'cnfs-pool-list',
   templateUrl: './pool-list.component.html',
@@ -11,9 +13,24 @@ export class PoolListComponent {
   public dataSource: CustomDataSource<IPool>;
 
   @Input()
-  public displayedColumns: string[] = ['name'];
+  public displayedColumns: string[] = ['name', 'actions'];
+
+  @Input()
+  public actions: IAction[] = [
+    { action: 'edit', icon: 'edit', label: 'Edit Pool' },
+  ];
+
+  @Output()
+  private action: EventEmitter<{
+    action: string;
+    pool: IPool;
+  }> = new EventEmitter();
 
   public constructor(customersAdapter: PoolsAdapter) {
     this.dataSource = new CustomDataSource(customersAdapter);
+  }
+
+  public onAction(action: string, pool: IPool): void {
+    this.action.next({ action, pool });
   }
 }
